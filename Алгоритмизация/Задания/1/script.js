5da7a32a5bdbb67b41075f9841155ad1d03867de
PatroviumStare <froschcode@gmail.com>
2026-09-15 04:54:25 +0600

    1.0
    1st task

--- Алгоритмизация/Задания/1/script.js
+++ Алгоритмизация/Задания/1/script.js
@@ -1,14 +1,23 @@
-const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
+
 let currentImageIndex = 0;
 
 const prevButton = document.querySelector("#prev-btn");
 const nextButton = document.querySelector("#next-btn");
-const mainImage = document.querySelector("#main-image");
-const image_list = document.querySelector("#image-list");
+
+const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
+const mainImage = $("#main-image");
+const imageList = $("#image-list");
+
+const form = document.querySelector("#feedback-form");
+const emailPattern = /.+@.+\..+/i;
+
+
+
 
 function updateMainImage() {
   mainImage.src = images[currentImageIndex];
 }
+
 
 prevButton.addEventListener("click", () => {
   if (currentImageIndex > 0) {
@@ -20,6 +29,7 @@ prevButton.addEventListener("click", () => {
   updateMainImage();
 });
 
+
 nextButton.addEventListener("click", () => {
     if (currentImageIndex < images.length - 1) {
         currentImageIndex++;
@@ -30,12 +40,13 @@ nextButton.addEventListener("click", () => {
     updateMainImage();
 });
 
+
 images.forEach((image, index) => {
     const listItem = document.createElement("li");
     const thumbnail = document.createElement("img");
 
     thumbnail.src = image;
-    thumbnail.width = 100;
+    thumbnail.width = 75;
 
     thumbnail.addEventListener("click", () => {
         currentImageIndex = index;
@@ -45,3 +56,71 @@ images.forEach((image, index) => {
     listItem.append(thumbnail);
     image_list.append(listItem);
 });
+
+
+function showMessage(message) {
+  $("#message-box")
+    .text(message)
+    .stop(true, true)
+    .slideDown(400);
+  
+    setTimeout(function () {
+      $("#message-box").slideUp(400);
+    }, 3000);
+}
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  const name  = $("#name").val().trim();
+  const email = $("#email").val().trim();
+  const formMessage = $("#message").val().trim();
+
+
+  if (name === "" || email === "" || formMessage === "") {
+    showMessage("Заполните все поля!");
+    return;
+  }
+  else if (!emailPattern.test(email)) {
+    showMessage("Неверный формат email-адреса!");
+    return;
+  }
+
+  $("#feedback-form")[0].reset();
+
+  showMessage("Сообщение отправлено!")
+
+}
+
+
+form.addEventListener("submit", formSubmit);
+
+
+function loadArticles() {
+    const articleList = $("#article-list");
+
+    $.getJSON("data.json", function (articles) {
+        $.each(articles, function (index, article) {
+
+            const listItem = $("<li>");
+
+            listItem.attr("id", article.id);
+
+            const title = $("<h2>")
+                .text(article.title);
+
+            const description = $("<p>")
+                .text(article.description);
+
+            listItem.append(title, description);
+            articleList.append(listItem);
+        });
+    });
+};
+
+document.addEventListener("dragstart", function (event) {
+    event.preventDefault();
+});
+
+
+window.onload = loadArticles;
